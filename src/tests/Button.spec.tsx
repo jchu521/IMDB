837edd876d29fb491d2c78ffa73b251317e694c8
bennyoppa <benny.j.shi@gmail.com>
2021-08-01 23:49:58 +1000

    fixed minor bugs

--- src/tests/Button.spec.tsx
+++ src/tests/Button.spec.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { mount } from "enzyme";
+import React from 'react';
+import { mount } from 'enzyme';
 
-
-test("hello world", () => {
-  const wrapper = mount(<p> text="111" </p>);
+test('hello world', () => {
+  const wrapper = mount(<p> text=&quot;111&quot; </p>);
   // expect(wrapper.text()).toMatch("Hello Jest!");
 
   expect(1).toEqual(1);
