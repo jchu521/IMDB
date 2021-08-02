78cc83c1e636a9d833ade589e25db79ff24ec01d
bennyoppa <benny.j.shi@gmail.com>
2021-08-03 02:03:49 +1000

    added MainCard component

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import MainCard from '../components/Cards/MainCard';
+
+export default function Test() {
+  return (
+    <div>
+      <MainCard />
+    </div>
+  );
+}
---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react';
+import MainCard from '../components/Cards/MainCard';
+
+storiesOf('Card', module).add('main card', () => {
+  return <MainCard />;
+});
